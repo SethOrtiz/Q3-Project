fc0ac146d8b057dd6f7112fb7ee0309e4b32b5c7
Krystine Jannenga <kjannenga1@gmail.com>
2019-07-25 12:27:26 -0700

    cards showing imgs from unsplash on search

--- client/src/components/PictureCard.js
+++ client/src/components/PictureCard.js
@@ -3,27 +3,28 @@
 import React from "react";
 import {CardText, Container } from "reactstrap";
 
-const PictureCard = () => {
+const PictureCard = (props) => {
 
   let imgStyle={
     width: "90%",
 
   };
+  console.log(props.url);
 
   return (
     <Container className="card m-5 " >
       < div className="row" >
         <div className="col-8">
           <div className="box">
-            <img src="https://bit.ly/2K41yGf" alt="img" className="card-size" style={imgStyle} />
-            <button className="btn btn-light">Button</button>
+            <img src={props.url} alt="img" className="card-size" style={imgStyle} />
+            <button className="btn btn-light" onClick={props.savePhoto}>Button</button>
           </div>
         </div>
         <div className='col-4'>
             <h5 className="">UserName</h5>
             <br/>
             <CardText>
-              {" "}
+              {props.id}
               Lorem ipsum placeat dicta voluptate itaque earum sit repudiandae.
             </CardText>
         </div>
