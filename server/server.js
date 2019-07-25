801eab6bb896bef999fb60c033d13b76b2314895
Krystine Jannenga <kjannenga1@gmail.com>
2019-07-25 14:20:54 -0700

    cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
+app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
