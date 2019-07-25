9fba5de420778811f7c5a37304db70800f0e2d36
Krystine Jannenga <kjannenga1@gmail.com>
2019-07-25 15:23:24 -0700

    images coming through to user profile after being saved

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,38 @@ import Profile from "./components/Profile";
 import Main from "./components/Main"
 import PrivateRoute from "./components/PrivateRoute";
 
-function App() {
-  return (
-      <div className="App">
-        <BrowserRouter>
-          <header>
-            <Navbar />
-          </header>
-          <Switch>
-            <Route path="/" component={Main} exact />
-            <PrivateRoute path="/profile" component={Profile} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-  );
+class App extends React.Component {
+
+    state = {
+        savedImgs:[]
+    }
+
+    componentDidMount = async () => {
+        const res = await fetch('http://localhost:8000/pictures');
+        const json = await res.json();
+        this.setState(prevState => ({
+            ...prevState,
+            savedImgs: json
+        }));
+        console.log(this.state)
+    };
+    render() {
+    return(
+
+<div className="App">
+<BrowserRouter>
+<header>
+<Navbar />
+</header>
+<Switch>
+<Route path="/" component={Main} exact />
+<PrivateRoute path="/profile" component={Profile} savedImgs={this.state.savedImgs} />
+</Switch>
+</BrowserRouter>
+</div>
+)
+    ;
+}
 }
 
 export default App;
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,20 +2,21 @@
 
 import React from "react";
 import { useAuth0 } from "../react-auth0-wrapper";
+import PictureList from './PictureList'
 import PictureCard from "./PictureCard"
 
-const Profile = () => {
+const Profile = (props) => {
+    console.log(props.savedImgs)
+
   let imgStyle = {
     borderRadius: "50%",
     height: "7em",
     width: "7em"
   };
   const { loading, user } = useAuth0();
-
   if (loading || !user) {
     return "Loading...";
   }
-
   return (
     <div>
       <img src={user.picture} alt="Profile" style={imgStyle} className="" />
@@ -27,21 +28,12 @@ const Profile = () => {
 
       <div>
         <h2>Saved Photos Here</h2>
+          <PictureList userId={user.sub}/>
         <div>
           <img src="https://unsplash.com/photos/peFx2NFgUFY" alt="" />
         </div>
       </div>
-      <div className="row">
-        <div className="col-5">
-          <PictureCard />
-        </div>
-        <div className="col-5">
-          <PictureCard />
-        </div>
-        <div className="col-5">
-          <PictureCard />
-        </div>
-      </div>
+      
     </div>
   );
 };
