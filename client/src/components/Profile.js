92b4d3d9cc3da3a5dfa3cc34f5fe042f0ed9c803
Seth Ortiz <sethl.ortiz@gmail.com>
2019-07-25 12:21:22 -0700

    add cards to profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,41 +2,48 @@
 
 import React from "react";
 import { useAuth0 } from "../react-auth0-wrapper";
-
+import PictureCard from "./PictureCard"
 
 const Profile = () => {
+  let imgStyle = {
+    borderRadius: "50%",
+    height: "7em",
+    width: "7em"
+  };
+  const { loading, user } = useAuth0();
 
-        let imgStyle={
-            borderRadius: "50%",
-            height: "7em",
-            width: "7em"
-        };
-        const { loading, user } = useAuth0();
+  if (loading || !user) {
+    return "Loading...";
+  }
 
+  return (
+    <div>
+      <img src={user.picture} alt="Profile" style={imgStyle} className="" />
+      <h2>name: {user.nickname}</h2>
+      <p>email: {user.email}</p>
+      <p>{user.sub}</p>
 
+      <code>{JSON.stringify(user, null, 2)}</code>
 
-        if (loading || !user) {
-        return "Loading...";
-    }
-
-    return (
+      <div>
+        <h2>Saved Photos Here</h2>
         <div>
-
-            <img src={user.picture} alt="Profile" style={imgStyle} className=''/>
-            <h2>name: {user.nickname}</h2>
-            <p>email: {user.email}</p>
-            <p>{user.sub}</p>
-
-            <code>{JSON.stringify(user, null, 2)}</code>
-
-            <div>
-                <h2>Saved Photos Here</h2>
-                <div>
-                    <img src="https://unsplash.com/photos/peFx2NFgUFY" alt=""/>
-                </div>
-            </div>
+          <img src="https://unsplash.com/photos/peFx2NFgUFY" alt="" />
         </div>
-    );
+      </div>
+      <div className="row">
+        <div className="col-5">
+          <PictureCard />
+        </div>
+        <div className="col-5">
+          <PictureCard />
+        </div>
+        <div className="col-5">
+          <PictureCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
