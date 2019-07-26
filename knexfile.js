5afff1ae85a529d2110decd98595df2ee9fc3265
Krystine Jannenga <kjannenga1@gmail.com>
2019-07-25 18:59:39 -0700

    Added ENV_VAR for Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 let connectionString = process.platform === 'win32' ? 'postgres://postgres:root@localhost/q3-api' : 'postgres://localhost/q3-api'
-  
+
   module.exports = {
     development: {
         client: 'pg',
-        connection: connectionString,
+        connection: process.env.DATABASE_URL || connectionString,
         migrations: {
             directory: __dirname + '/db/migrations',
           },
